c725759f32bc88d2431846c7019ea7323e705b54
k.zinovyev <k.zinovyev@rambler-co.ru>
2017-01-17 16:51:57 +0300

    added target tvOS

---
+++ Example/Pods/Pods-Example-TVos/Pods_Example_TVos.h
@@ -0,0 +1,13 @@
+//
+//  Pods_Example_TVos.h
+//  Pods-Example-TVos
+//
+//  Created by k.zinovyev on 17.01.17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pods_Example_TVos : NSObject
+
+@end
---
+++ Example/Pods/Pods-Example-TVos/Pods_Example_TVos.m
@@ -0,0 +1,13 @@
+//
+//  Pods_Example_TVos.m
+//  Pods-Example-TVos
+//
+//  Created by k.zinovyev on 17.01.17.
+//
+//
+
+#import "Pods_Example_TVos.h"
+
+@implementation Pods_Example_TVos
+
+@end
